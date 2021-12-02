6932b56f001c9cb2f0fbea3fbcab9c958aa69fe1
Jacob Chambers <2330928@live.barnsley.ac.uk>
2021-12-02 12:34:03 +0000

    Documentation 1

--- EasySharpIni/Models/IniField.cs
+++ EasySharpIni/Models/IniField.cs
@@ -10,13 +10,27 @@ namespace EasySharpIni.Models
         private readonly IniSection? _section;
 
         private string _key;
+        /// <summary>
+        /// The Key name of the field.
+        /// </summary>
         public string Key => _key;
 
         private string _rawValue;
 
         private readonly string _defaultValue;
+        /// <summary>
+        /// The Default value of the field. 
+        /// </summary>
         public string DefaultValue => _defaultValue;
 
+        /// <summary>
+        /// Creates a new <see cref="IniField"/> with a key and <see cref="IniFile"/> reference.
+        /// </summary>
+        /// <param name="key">The key of this field.</param>
+        /// <param name="file">The <see cref="IniFile"/> this field is contained in.</param>
+        /// <param name="value">The value to assign this field.</param>
+        /// <param name="defaultValue">The value this field should take by default if no value is assigned.</param>
+        /// <param name="section">The section this field belongs to.</param>
         public IniField(string key, IniFile file, string? value = null, string defaultValue = "", string section = "")
         {
             _key = key;
@@ -32,11 +46,22 @@ namespace EasySharpIni.Models
                 _rawValue = value;
         }
 
+        /// <summary>
+        /// Returns the raw string value of the field.
+        /// </summary>
+        /// <returns></returns>
         public string Get()
         {
             return _rawValue;
         }
 
+        /// <summary>
+        /// Converts the value of the field to type <typeparamref name="T"/> using the supplied <see cref="IConverter{T}"/>.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="IConverter"></typeparam>
+        /// <param name="converter"></param>
+        /// <returns></returns>
         public T Get<T>(IConverter<T> converter)
         {
             bool success = converter.Parse(_rawValue, out T result);
@@ -46,6 +71,10 @@ namespace EasySharpIni.Models
             return result;
         }
 
+        /// <summary>
+        /// Sets the raw string value of the field.
+        /// </summary>
+        /// <param name="value"></param>
         public void Set(string value)
         {
             _rawValue = value;
